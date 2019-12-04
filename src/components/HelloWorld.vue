cd87dd9243a259621d67009071a6acf08271f36e
Jeremy Baer <jeremy.baer@capgemini.com>
2019-12-04 10:00:56 +0100

    add bulma and sass configuration

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="is-family-code">
+    <h1 class="title is-1 has-text-danger">{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
