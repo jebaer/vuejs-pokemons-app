63cdba4092fc4748d5507bbaef18ac2e65cff185
Jeremy Baer <jeremy.baer@capgemini.com>
2019-12-20 16:25:04 +0100

    create pokemoncard component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="" id="app">
     <myHeader></myHeader>
+    <PokemonCard></PokemonCard>
   </div>
 </template>
 
 <script>
-import myHeader from './components/Header.vue'
+import myHeader from './components/Header.vue';
+import PokemonCard from './components/PokemonCard.vue';
+
 export default {
   name: 'app',
   components: {
-    myHeader
+    myHeader,
+    PokemonCard
   }
 }
 </script>
 
 <style lang="sass">
-
+body
+  font-size: 12px
+  font-family: sans-serif
+  color: #000
 </style>
---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="container">
+
+    <div class="is-mobile columns pokemon-card is-vcentered">
+
+      <div class="column">
+        <img :src="pokemon[0].image"/>
+      </div>
+
+      <div class="column">
+        <div class="is-flex">
+          <p id="pokemon-id">#{{ pokemon[0].id }}</p>
+          <p class="has-text-link is-uppercase is-bold">{{ pokemon[0].name }}</p>
+        </div>
+        <p>Poids: {{ pokemon[0].height}}</p>
+        <p>Taille: {{ pokemon[0].weight }}</p>
+        <p>Type: {{ pokemon[0].types }}</p>
+        <p>lvl {{ pokemon[0].evolve.level }}: {{ pokemon[0].evolve["evolve-to"] }}
+          <span class="evol-pokemon"><img :src="pokemon[0].evolve.image" width="25px" height="25px"/></span>
+        </p>
+      </div>
+
+    </div>
+
+  </section>
+</template>
+
+<script>
+import Pokemons from "../assets/mock_pokemons.json"
+
+export default {
+  name: "pokemonCard",
+  data() {
+    return {
+      pokemon: Pokemons
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+  .pokemon-card
+    margin: 25px
+    background-color: lighten(#E6EBE0, 5%)
+
+  #pokemon-id
+    margin-right: 15px
+</style>
