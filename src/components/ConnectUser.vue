b3a5ecd79e169d0c41938f12e48bd8addd7dbb62
Jeremy Baer <jeremy.baer@capgemini.com>
2019-12-20 10:36:15 +0100

    refactore header.vue

---
+++ src/components/ConnectUser.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="column has-text-centered">
+    <div @click="connectUser()" class="is-inline-flex">
+      <span class="circle-wrapper"  v-bind:class="{connected: connected}"></span>
+    </div>
+    <p  class="has-text-link is-half">
+      <span v-if="connected">{{ playerName }}</span>
+      <span v-else> Inconnu </span>
+    </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "ConnectUser",
+  data() {
+    return {
+      playerName: "Sacha",
+      connected: false
+    }
+  },
+  methods: {
+    connectUser(){
+      this.connected = !this.connected;
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+  span.circle-wrapper
+    min-width: 30px
+    max-width: 30px
+    min-height: 30px
+    border: solid 3px #ED6A5A
+    border-radius: 50%
+    background-color:  #E6EBE0
+  span.connected
+    border: solid 3px #03CEA4
+  p
+    min-height: 25px
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="column is-three-quarters has-text-left">
+  <h1 class="has-text-link is-size-4 is-family-primary"> Pokevue!</h1>
+  <h2 class="has-text-light is-size-6 is-family-primary"> Attrapez les tous. </h2>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: "Title",
+}
+</script>
