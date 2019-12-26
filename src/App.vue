3b1c7c512158c7028ce8704e45a4336d3b674330
Jeremy Baer <jeremy.baer@capgemini.com>
2019-12-26 16:02:10 +0100

    first component myheader

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="grey-background" id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyHeader></MyHeader>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyHeader from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MyHeader
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+<header class="is-flex-row blue-background is-justify-between">
+
+  <div class="is-flex-column is-align-right ">
+    <h1 class="is-first-title"> Pokevue!</h1>
+    <h2 class="is-second-title"> Attrapez les tous. </h2>
+  </div>
+
+  <div class="is-flex-column is-align-centered is-justify-center">
+    <div @click="connectUser()" class="clickable">
+      <span class="circle-wrapper"  v-bind:class="{connected: connected}"></span>
+    </div>
+    <p  class="is-text-special no-margin">
+      <span v-if="connected">{{ playerName }}</span>
+      <span v-else> Inconnu </span>
+    </p>
+  </div>
+</header>
+</template>
+
+<script>
+  export default {
+    name: "myHeader",
+    data() {
+      return {
+        playerName: "Sacha",
+        connected: false
+      }
+    },
+    methods: {
+      connectUser(){
+        this.connected = !this.connected;
+      }
+    }
+  }
+</script>
+
+<style lang="sass">
+  div
+    min-width: 70px
+
+    span.circle-wrapper
+      display: inline-flex
+      margin-left: 17px
+      min-width: 30px
+      max-width: 30px
+      min-height: 30px
+      border: solid 3px #ED6A5A
+      border-radius: 50%
+      background-color:  #E6EBE0
+    span.connected
+      border: solid 3px #03CEA4
+    p
+      min-height: 25px
+</style>
